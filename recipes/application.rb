6c11a8ce232fc6994c2da41761038f8f29d9ee65
Sukach <sukach.v.v@gmail.com>
2015-11-20 09:23:24 +0200

    fix

--- recipes/application.rb
+++ recipes/application.rb
@@ -2,6 +2,7 @@ include_recipe 'build-essential::default'
 include_recipe 'python::pip'
 include_recipe 'python::virtualenv'
 
+package 'git'
 package 'postgresql-devel'
 package 'python-devel'
 package 'openssl-devel'
